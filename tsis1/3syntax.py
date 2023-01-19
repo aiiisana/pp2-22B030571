84a65cb0dc0bc2a109343e30b6b096af5ebf76c1
aisana <a_ondassyn@kbtu.kz>
2023-01-19 20:01:13 +0600

    added all tasks

---
+++ tsis1/3syntax.py
@@ -0,0 +1,26 @@
+#first example
+if 5 > 2:
+    print("Five is greater than two!")
+
+
+
+#second example
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+
+
+#third example
+x = 5
+y = "Hello, World!"
+
+
+
+#4th example
+#This is a comment.
+print("Hello, World!")
+
+#1exersize
+print("Hello, World!")
