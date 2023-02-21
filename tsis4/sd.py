1df317850f133186ab71f0dfbbdb380598aea003
aisana <a_ondassyn@kbtu.kz>
2023-02-21 10:23:43 +0600

    added changes

--- tsis4/sd.py
+++ tsis4/sd.py
@@ -2,7 +2,12 @@ import json
 
 with open('sample-data.json', 'r') as f:
     data = json.load(f)
-
+print("""Interface Status
+================================================================================
+DN                                                 Description           Speed    MTU  
+-------------------------------------------------- --------------------  ------  ------""")
 if 'imdata' in data:
     for x in data['imdata']:
-        print(x['l1PhysIf']['attributes']['dn'])
+        print(x['l1PhysIf']['attributes']['dn'], end= '                               ')
+        print(x['l1PhysIf']['attributes']['speed'], end= '  ')
+        print(x['l1PhysIf']['attributes']['mtu'], end= '\n')
